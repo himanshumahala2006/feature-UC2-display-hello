e19d8a29aa0baf9d5483d5966850f0ac119623d5
himanshumahala2006 <thehimanshumahala@gmail.com>
2026-03-22 06:26:09 +0530

    UC2: Added feature to display name using command line argument

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Check if name is passed
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
     }
 }
